7255a6fa566f7f22691580b999f88aad4873cab1
Masashi SAKAI <masashi.sakai1986@gmail.com>
2015-03-06 01:31:49 +0900

    POJ#3617: add line break

--- poj/3617_best_cow_line.cpp
+++ poj/3617_best_cow_line.cpp
@@ -5,26 +5,32 @@ using namespace std;
 
 const unsigned MAX_N = 2000;
 
-void solve(int n, const char *original_ptr, char *new_ptr)
+void solve(int n, const char *original_ptr)
 {
+    char new_line[81] = {0};
     char reverse_line[MAX_N + 1] = {0};
     for (int i = 0; i < n; ++i) {
         reverse_line[i] = original_ptr[n - 1 - i];
     }
 
+    int i = 0;
     char *reverse_ptr = reverse_line;
-    while (0 < n) {
+    for (; 0 < n; ++i, --n) {
+        if (i == 80) {
+            cout << new_line << endl;
+            i = 0;
+        }
         int cmp = strcmp(original_ptr, reverse_ptr);
         if (cmp < 0) {
-            *new_ptr = *original_ptr;
+            new_line[i] = *original_ptr;
             ++original_ptr;
         } else {
-            *new_ptr = *reverse_ptr;
+            new_line[i] = *reverse_ptr;
             ++reverse_ptr;
         }
-        --n;
-        ++new_ptr;
     }
+    new_line[i] = '\0';
+    cout << new_line << endl;
 }
 
 int main()
@@ -37,9 +43,7 @@ int main()
         cin >> cow_line[i];
     }
 
-    char new_line[MAX_N + 1] = {0};
-    solve(n, cow_line, new_line);
-    cout << new_line << endl;
+    solve(n, cow_line);
 
     return 0;
 }
